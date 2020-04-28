71f3cf00eeda13a46cb1f42d3db45567f92b1f15
Sidheswar Ghosh <sidheswar.ghosh@gmail.com>
2020-04-28 15:09:14 +0200

    Added recursion functions

---
+++ src/recursion.py
@@ -0,0 +1,35 @@
+
+# Count down with recusion
+def countdown(x):
+    if x==0:
+        return
+    else:
+        print("x:", x)
+        countdown(x-1)
+
+# Factorial with recusion 
+def factorial(n):
+    if n == 0:
+        return 1  
+    if n == 1:                
+        return n
+    else:           
+        return n*factorial(n-1)
+
+# Fibonacci with recursion
+def fibonacci(n):
+    if n<=1:
+        return n
+    else:
+        return(fibonacci(n-1) + fibonacci(n-2))  
+
+
+countdown(5)
+fact = factorial(5)
+print("Factorial: ",fact)
+
+n = 10
+print("Fibonacce Series: ", n)
+for i in range(n):
+    fib = fibonacci(i)
+    print(fib)
